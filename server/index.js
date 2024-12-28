5e5994eec4b07e626ebc4795586179e941573408
Rupa-Rd <roopa.69.spr@gmail.com>
2024-12-28 14:31:34 +0530

    MongoDB is connected

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import generalRoutes from './routes/general.js';
-import customerRoutes from './routes/client.js';
+import customerRoutes from './routes/customer.js';
 import orderRoutes from './routes/order.js';
 import menuRoutes from './routes/menu.js';
 import settingRoutes from './routes/setting.js';
@@ -28,3 +28,14 @@ app.use("/customer", customerRoutes);
 app.use("/order", orderRoutes);
 app.use("/menu", menuRoutes);
 app.use("/setting", settingRoutes);
+
+// console.log("Server is running...");
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() =>{
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+}).catch((error) => console.log(`${error} did not connect`))
