774bd84c95d7820b236f55c69a50386a8936a5d4
Rupa-Rd <roopa.69.spr@gmail.com>
2024-12-29 21:34:54 +0530

    A change

--- client/src/states/api.js
+++ client/src/states/api.js
@@ -1,7 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+console.log(process.env.REACT_APP_BASE_URL);
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL || "https://admin-dashboard-server-mssm.onrender.com"}),
   reducerPath: "adminApi",
   tagTypes: ["User", "Customers", "Menu", "Orders"],
   endpoints: (build) => ({
