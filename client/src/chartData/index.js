0eb8fc8afff6712108ca14d4605374faebc7c6b6
Rupa-Rd <roopa.69.spr@gmail.com>
2024-12-29 21:18:45 +0530

    Chardata

---
+++ client/src/chartData/index.js
@@ -0,0 +1,99 @@
+export const profitData2021 = [
+  { month: "January", profit_in_inr: 80000 },
+  { month: "February", profit_in_inr: 90000 },
+  { month: "March", profit_in_inr: 100000 },
+  { month: "April", profit_in_inr: 110000 },
+  { month: "May", profit_in_inr: 120000 },
+  { month: "June", profit_in_inr: 130000 },
+  { month: "July", profit_in_inr: 140000 },
+  { month: "August", profit_in_inr: 150000 },
+  { month: "September", profit_in_inr: 160000 },
+  { month: "October", profit_in_inr: 180000 },
+  { month: "November", profit_in_inr: 190000 },
+  { month: "December", profit_in_inr: 200000 },
+];
+
+export const profitData2022 = [
+  { month: "January", profit_in_inr: 50000 },
+  { month: "February", profit_in_inr: 55000 },
+  { month: "March", profit_in_inr: 60000 },
+  { month: "April", profit_in_inr: 65000 },
+  { month: "May", profit_in_inr: 70000 },
+  { month: "June", profit_in_inr: 75000 },
+  { month: "July", profit_in_inr: 80000 },
+  { month: "August", profit_in_inr: 85000 },
+  { month: "September", profit_in_inr: 90000 },
+  { month: "October", profit_in_inr: 100000 },
+  { month: "November", profit_in_inr: 110000 },
+  { month: "December", profit_in_inr: 120000 },
+];
+
+export const profitData2023 = [
+  { month: "January", profit_in_inr: 120000 },
+  { month: "February", profit_in_inr: 130000 },
+  { month: "March", profit_in_inr: 140000 },
+  { month: "April", profit_in_inr: 145000 },
+  { month: "May", profit_in_inr: 150000 },
+  { month: "June", profit_in_inr: 160000 },
+  { month: "July", profit_in_inr: 170000 },
+  { month: "August", profit_in_inr: 180000 },
+  { month: "September", profit_in_inr: 190000 },
+  { month: "October", profit_in_inr: 200000 },
+  { month: "November", profit_in_inr: 220000 },
+  { month: "December", profit_in_inr: 230000 },
+];
+
+export const profitData2024 = [
+  { month: "January", year: 2024, profit_in_inr: 150000 },
+  { month: "February", year: 2024, profit_in_inr: 170000 },
+  { month: "March", year: 2024, profit_in_inr: 180000 },
+  { month: "April", year: 2024, profit_in_inr: 190000 },
+  { month: "May", year: 2024, profit_in_inr: 210000 },
+  { month: "June", year: 2024, profit_in_inr: 220000 },
+  { month: "July", year: 2024, profit_in_inr: 210000 },
+  { month: "August", year: 2024, profit_in_inr: 200000 },
+  { month: "September", year: 2024, profit_in_inr: 215000 },
+  { month: "October", year: 2024, profit_in_inr: 225000 },
+  { month: "November", year: 2024, profit_in_inr: 230000 },
+  { month: "December", year: 2024, profit_in_inr: 230000 },
+];
+
+export const popularTimeData = [
+  {
+    day: "2024-01-01",
+    hours: [
+      { hour: "10 AM", activity: 50 },
+      { hour: "11 AM", activity: 60 },
+      { hour: "12 PM", activity: 70 },
+      { hour: "1 PM", activity: 80 },
+      { hour: "2 PM", activity: 90 },
+      { hour: "3 PM", activity: 100 },
+      { hour: "4 PM", activity: 110 },
+      { hour: "5 PM", activity: 120 },
+      { hour: "6 PM", activity: 130 },
+      { hour: "7 PM", activity: 140 },
+      { hour: "8 PM", activity: 150 },
+      { hour: "9 PM", activity: 160 },
+      { hour: "10 PM", activity: 170 },
+    ]
+  },
+  {
+    day: "2024-01-02",
+    hours: [
+      { hour: "10 AM", activity: 55 },
+      { hour: "11 AM", activity: 65 },
+      { hour: "12 PM", activity: 75 },
+      { hour: "1 PM", activity: 85 },
+      { hour: "2 PM", activity: 95 },
+      { hour: "3 PM", activity: 105 },
+      { hour: "4 PM", activity: 115 },
+      { hour: "5 PM", activity: 125 },
+      { hour: "6 PM", activity: 135 },
+      { hour: "7 PM", activity: 145 },
+      { hour: "8 PM", activity: 155 },
+      { hour: "9 PM", activity: 165 },
+      { hour: "10 PM", activity: 175 },
+    ]
+  },
+  // Add more days as needed
+];
